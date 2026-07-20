f07e2dc4bd4daa9dbc1761584258cdfa628ef0af
sensen02 <sensen02@github.com>
2026-07-20 09:30:38 +0800

    chore: remove deprecated diagnostic and test scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !! 永不删除 checkpoint.pt !!
-# !! 脚本自带 resume，崩了从最近存档继续，不丢进度 !!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-cd /home/linux/srcn_v2
-export PATH="/home/linux/anaconda3/bin:$PATH"
-SRCN_B=${SRCN_B:-96} torchrun --nproc_per_node=4 train_multi.py > train.log 2>&1
--- run_fixed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Fixed DDP/truncated-BPTT training copy. Keeps checkpoints inside this directory.
-set -euo pipefail
-cd /home/linux/srcn_v2_fixed
-export PATH="/home/linux/anaconda3/bin:$PATH"
-SRCN_B=${SRCN_B:-96} torchrun --nproc_per_node=4 train_multi.py > train.log 2>&1
